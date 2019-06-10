097a610da38311b6c66fc253df1740e60aa377a2
Mukul Singh <mukulsingh1248@gmail.com>
2019-06-11 01:47:41 +0530

    trimmed css

--- generaltech/generaltech/views.py
+++ generaltech/generaltech/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import Http404
 
+import datetime
 import requests
 
 def index(request):
     result = requests.get('https://api.pinkadda.com/v1/posts/published?project=pinkadda&limit=10&offset=0')
-    context = {}
+    date = getDate()
+    context = {'date': date}
     return render(request, 'generaltech/index.html', context)
 
 def article(request, article_id):
@@ -14,3 +16,7 @@ def article(request, article_id):
 
 def writer(request, writer_id):
     pass
+
+def getDate():
+    date = datetime.datetime.now()
+    return date.strftime("%A, %d %b %Y")
