785b5c524e3530925ab9e8fb60b299b7c5cfb9a7
Mukul Singh <mukulsingh1248@gmail.com>
2019-06-11 22:47:37 +0530

    switched to grid layout

--- generaltech/generaltech/views.py
+++ generaltech/generaltech/views.py
@@ -5,7 +5,7 @@ import datetime
 import requests
 
 def index(request):
-    result = requests.get('https://api.pinkadda.com/v1/posts/published?project=pinkadda&limit=10&offset=0')
+    # result = requests.get('https://api.pinkadda.com/v1/posts/published?project=pinkadda&limit=10&offset=0')
     date = getDate()
     context = {'date': date}
     return render(request, 'generaltech/index.html', context)
