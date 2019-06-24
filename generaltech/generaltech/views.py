555fcad2bdce57fd50063990a459d05f296bf9f9
Mukul Singh <mukulsingh1248@gmail.com>
2019-06-25 01:37:34 +0530

    connected to api and tweaked layout

--- generaltech/generaltech/views.py
+++ generaltech/generaltech/views.py
@@ -5,9 +5,19 @@ import datetime
 import requests
 
 def index(request):
-    # result = requests.get('https://api.pinkadda.com/v1/posts/published?project=pinkadda&limit=10&offset=0')
+    response = requests.get('https://api.pinkadda.com/v1/posts/published?project=pinkadda&limit=10&offset=0')
+    response_dict = response.json()
+    posts = response_dict['posts']
+    posts = posts[3:] #fix_me remove this later
+    featured_posts = response_dict['featured']
+    main_article = featured_posts[0] #fix_me after main_article post type is defined in the api
     date = getDate()
-    context = {'date': date}
+    context = {
+        'date': date,
+        'posts': posts,
+        'featured_posts': featured_posts,
+        'main_article': main_article,
+    }
     return render(request, 'generaltech/index.html', context)
 
 def article(request, article_id):
