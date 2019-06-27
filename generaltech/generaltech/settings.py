ff57237c6a7bf30635388297fff3305022be8c1f
Mukul Singh <mukulsingh1248@gmail.com>
2019-06-27 19:12:36 +0530

    added date,  changed font,  reorganised header

--- generaltech/generaltech/settings.py
+++ generaltech/generaltech/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '01jn7r9!$twyay-1yqf0xsg*b$vh8slhnh^vmb)!b79y6x00t)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]','172.20.10.5']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]','172.20.10.5','192.168.43.163']
 
 
 # Application definition
--- generaltech/generaltech/views.py
+++ generaltech/generaltech/views.py
@@ -3,30 +3,60 @@ from django.http import Http404
 
 import datetime
 import requests
+from requests.exceptions import HTTPError
 
 def index(request):
-    response = requests.get('https://api.pinkadda.com/v1/posts/published?project=pinkadda&limit=10&offset=0')
-    response_dict = response.json()
-    posts = response_dict['posts']
-    posts = posts[3:] #fix_me remove this later
-    featured_posts = response_dict['featured']
-    main_article = featured_posts[0] #fix_me after main_article post type is defined in the api
     date = getDate()
-    context = {
-        'date': date,
-        'posts': posts,
-        'featured_posts': featured_posts,
-        'main_article': main_article,
-    }
+    context = { 'date': date, }
+    try:
+        response = requests.get(
+            'https://api.pinkadda.com/v1/posts/published',
+            params={
+                'project': 'pinkadda',
+                'limit': '10',
+                'offset': '0'
+            }
+        )
+        response.raise_for_status()
+    except HTTPError as http_error:
+        print(f'HTTP error occured: {http_error}')
+        raise Http404("Page does not exist")
+    except Exception as err:
+        print(f'Other error occured: {err}')
+        raise Http404("Page does not exist")
+    else:
+        response_dict = response.json()
+        posts = response_dict['posts']
+        posts = list(map(formatPost, posts))
+        posts = posts[3:] #fix_me remove this later
+        featured_posts = response_dict['featured']
+        featured_posts = list(map(formatPost, featured_posts))
+        main_article = featured_posts[0] #fix_me after main article post type is defined in the api
+        context['posts'] = posts
+        context['featured_posts'] = featured_posts
+        context['main_article'] = main_article
     return render(request, 'generaltech/index.html', context)
 
 def article(request, article_id):
-    #raise Http404("Article not found")
-    pass
+    raise Http404("Article not found")
 
 def writer(request, writer_id):
-    pass
+    raise Http404("Article not found")
 
 def getDate():
-    date = datetime.datetime.now()
-    return date.strftime("%A, %d %b %Y")
+    d = datetime.datetime.now()
+    return d.strftime("%A, %d %b %Y")
+
+def formatPost(post):
+    post['created_on'] = formatCreationDate(post['created_on'])
+    return post
+
+def formatCreationDate(timestamp):
+    # Timestamp coverted into miliseconds for compatibility
+    d = datetime.datetime.fromtimestamp(timestamp/1000)
+    if d.date() == datetime.date.today():
+        return "Today at " + d.strftime("%I:%M %p")
+    elif d.date() == datetime.date.today() - datetime.timedelta(days=1):
+        return "Yesterday at " + d.strftime("%I:%M %p")
+    else:
+        return d.strftime("%b %d, %Y")
