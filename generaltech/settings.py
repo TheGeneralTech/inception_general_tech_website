ec2a3a2afc851db0b67aa8e2ea0beae0fb916667
Mukul Singh <mukulsingh1248@gmail.com>
2020-03-22 00:31:17 +0530

    can add multiple hosts now

--- generaltech/settings.py
+++ generaltech/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config.get('django','SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [config.get('django','HOST')]
+ALLOWED_HOSTS = list(config.get('django','HOST').split(','))
 
 
 # Application definition
